89d5fd621ff47f2cc6190cdef254932f6e3cc264
Yewert <blazer.andrew@gmail.com>
2018-11-11 14:23:57 +0500

    d

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os
 import telebot
 
+from telebot.types import Message
 from dialog.engine import DialogEngine
 
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG, filename='bot.log')
 logger = logging.getLogger(__name__)
 
 
@@ -16,17 +17,22 @@ bot = telebot.TeleBot(telegram_api_key)
 
 
 @bot.message_handler(func=lambda m: True)
-def lambda_handler(event):
+def lambda_handler(message):
     try:
-        if event.content_type != 'text':
-            logger.debug(event)
-        answer = dialog_engine.choose_answer(event)
+        if message is not Message:
+            return
+        if message.content_type != 'text':
+            logger.debug(message)
+        answer = dialog_engine.choose_answer(message)
         if answer:
-            bot.reply_to(event, answer)
+            bot.reply_to(message, answer)
 
     except Exception as e:
+        logger.warning(e)
+
+
+while True:
+    try:
+        bot.polling()
+    except Exception as e:
         logger.error(e)
-
-
-bot.polling()
-
